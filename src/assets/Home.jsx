3e53be108ad955ef2728074156612ad3ae1a606e
AngrySusanoo <lunadev823@gmail.com>
2023-03-16 13:26:07 -0400

    modify about

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Header from "../components/Header"
-import Contact from "../components/Contact"
-import Footer from "../components/Footer"
-import Projects from "../components/Projects"
-import Skills from "../components/Skills"
+import React from "react";
+import Header from "../components/Header";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import AboutMe from "../components/Aboutme";
 
 function Home() {
   return (
     <>
-     <Header/> 
-     <Skills/> 
-     <Projects/>
-     <Contact/>
-     <Footer/>
+      <Header />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <>
+      <div className="w-full flex">
+        <span className="w-10">Hello</span>
+      </div>
+    </>
+  );
+};
+export default AboutMe;
